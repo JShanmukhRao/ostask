8ab8963e44bfdc594564a69dc50e2f0b4395bb2f
JShanmukhRao <49684626+JShanmukhRao@users.noreply.github.com>
2019-04-17 23:46:59 +0530

    Add files via upload

---
+++ os3.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,n,j=0,k=0,x=0,l,req[50],mov=0,cp,ub,end,pv, lower[50],upper[50], temp,a[50];
+    printf("Enter total number of cylinders \n");
+    scanf("%d",&ub);
+	printf("Enter the current position\n");
+    scanf("%d",&cp);
+    printf("Enter the previous position\n");
+    scanf("%d",&pv);
+    printf("Enter the number of elements of the queue\n");
+    scanf("%d",&n);
+    printf("Enter the request queue\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&req[i]);
+    }
+    
+    
+    for(i=0;i<n;i++)
+    {
+        if(req[i]<cp)
+        {
+            lower[j]=req[i];
+            j++;
+        }
+        if(req[i]>cp)
+        {
+            upper[k]=req[i];
+            k++;
+        }
+    }
+    
+}
