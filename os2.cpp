123b2cc189cf158d852656e3caabbee61273c5ff
JShanmukhRao <49684626+JShanmukhRao@users.noreply.github.com>
2019-04-17 23:46:14 +0530

    Add files via upload

---
+++ os2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,n,j=0,k=0,x=0,l,req[50],mov=0,cp,ub,end,pv, lower[50],upper[50], temp,a[50];
+    printf("Enter total number of cylinders \n");
+    scanf("%d",&ub);
+	printf("Enter the current position\n");
+    scanf("%d",&cp);
+    printf("Enter the previous position\n");
+    scanf("%d",&pv);
+    printf("Enter the number of elements of the queue\n");
+    scanf("%d",&n);
+    printf("Enter the request queue\n");
+}
